f430dbd8406ebd17ef093dc84ef13c53942e8911
myccss <qq540007404@gmail.com>
2026-06-25 21:01:42 +0800

    init5

--- src/main/resources/com/google/jenkins/plugins/computeengine/ComputeEngineCloud/provision.js
+++ src/main/resources/com/google/jenkins/plugins/computeengine/ComputeEngineCloud/provision.js
@@ -1,8 +1,16 @@
 Behaviour.specify("[data-type='gcloud-provision']", 'gcloud-provision', -99, function(e) {
   e.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
     const form = document.getElementById(e.dataset.form);
-    form.querySelector("[name='configuration']").value = e.dataset.url;
+    form.querySelector("[name='configuration']").value = e.dataset.configuration;
     buildFormTree(form);
-    form.submit();
+
+    if (typeof form.requestSubmit == "function") {
+      form.requestSubmit();
+      return;
+    }
+
+    form.querySelector("button[type='submit']").click();
   });
 });
